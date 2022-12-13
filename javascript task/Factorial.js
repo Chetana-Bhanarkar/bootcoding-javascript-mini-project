50963c3e6bbc0c827d90b2e38b7aa1a826bd124e
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-13 16:13:25 +0530

    Factorial

--- javascript task/Factorial.js
+++ javascript task/Factorial.js
@@ -1,7 +1,31 @@
-let fact = 1;
-let n = 5 ; 
-for(let i=1 ;i<=n; i++){
-    fact = fact*i;
+// Write a program to find the factorial of n, n-1, n-2, n-3, n-4 ?
+
+// let n = 12 ; 
+// let fact =1;
+// for(let i = (n-4) ; i <= n ; i++){
+//     fact = fact * i ; 
+// }
+
+// console.log(fact);
+const method = () => {
+    let n = 12 ;
+    for(let i= (n-4); i<=n ; i++){
+        fact();
+    }
 }
 
-console.log(fact);
+const fact = () =>{
+    let n; 
+    if ( n <=0 ){
+        console.log('number should not be zero');
+    }
+    else{
+        let fact = 1;
+        for(let i = 1 ; i<=n ; i++){
+            fact = fact * i ;
+        }
+        return (`${n} : ${fact}`);
+    }
+}
+
+console.log(method());
