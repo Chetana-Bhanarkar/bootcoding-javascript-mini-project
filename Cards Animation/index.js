d637207a29fe0bee7b043b4d50530c4788b74e49
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-02-24 14:12:55 +0530

    animated cards

--- Cards Animation/index.js
+++ Cards Animation/index.js
@@ -3,8 +3,7 @@ const panels = document.querySelectorAll(".panel");
 //function to add class
 panels.forEach(panel=>{
     panel.addEventListener("click",()=>{
-        //call remove function
-        remove();
+        remove();    //call remove function   
         panel.classList.add('active');
     });
 });
