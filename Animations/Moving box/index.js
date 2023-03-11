225dc0cd52db7236fd01699e1e6f9145eca99912
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-03-11 23:46:42 +0530

    js file

--- Animations/Moving box/index.js
+++ Animations/Moving box/index.js
@@ -1,3 +1,27 @@
-const move = () => {
-    let redElement = 
-}
+    let btn = document.querySelector("#btn");
+    let redElement = document.querySelector("#red");
+// const move = () => {
+//     let poe = 0 //position of element
+//     let anime = setInterval()
+
+
+//     function animate(){
+//         poe++;
+//         redElement.style
+//     }
+// }
+btn.addEventListener("click",()=>{
+    let poe = 0;
+    let anime = setInterval(animate,5);
+    
+    function animate(){
+        if(poe == 400){
+            clearInterval(anime);
+        }else{
+            poe++;
+            redElement.style.top = poe+"px";
+            redElement.style.left = poe+"px";
+        }
+    }
+})
+
