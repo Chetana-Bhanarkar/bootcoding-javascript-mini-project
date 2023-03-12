0a2de2a86e27b88bee3a5a52f33ec4249519273a
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-03-12 13:51:25 +0530

    js file

--- Animations/Moving box/index.js
+++ Animations/Moving box/index.js
@@ -24,7 +24,7 @@ btn.addEventListener("click", () => {
 
     function animee() {
         if (poe == 400) {
-            
+            clearInterval(anim);
         } else {
             poe++;
             yellowElement.style.bottom = poe + "px";
