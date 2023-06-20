89671d37595b7ead73f9e9cd9c18345f90dde269
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-20 13:49:25 +0530

    example 2 of closure

---
+++ javascript task/Javascript Topics/closure/example2.js
@@ -0,0 +1,6 @@
+// closure definition :
+
+// we can create function inside function and we can access the variable of outer function in inner function but we cannot access the variable of innerfunction in outerfunction , it will throw error.
+
+
+
